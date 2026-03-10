5b47e11faa41cf357ba580c82448a342a785b18a
Steve McDougall <juststevemcd@gmail.com>
2026-03-10 12:08:48 +0000

    chore: fixing build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import svelte from '@astrojs/svelte';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://linux.juststeveking.com',
-
+  output: 'static',
+  adapter: cloudflare(),
   integrations: [svelte()]
 });
